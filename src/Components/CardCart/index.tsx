ab8a4bed070c5c8a194ae3c866f97778ebed1025
Camila Gonçalves Domingos Tavares <105559819+eucamila22@users.noreply.github.com>
2023-03-14 14:24:05 -0300

    fix/cart counter

--- src/Components/CardCart/index.tsx
+++ src/Components/CardCart/index.tsx
@@ -1,40 +1,38 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { LiCard } from './style'
 import { BiTrash } from 'react-icons/bi'
 import { CartContext } from '../../Contexts/CartContext'
 
-
 interface iElemProps {
-  elem: {
-     id: number
-     img: string
-     name: string
-     price: number
-  }
+    elem: {
+        id: number
+        img: string
+        name: string
+        price: number
+    }
 }
 
-const CardCart = ({elem}: iElemProps) => {
+const CardCart = ({ elem }: iElemProps) => {
+    const { removeCard } = useContext(CartContext)
 
-  const { removeCard } =useContext(CartContext)
-
-  return (
-    <LiCard>
-      <div className='divImg'>
-        <img src={elem.img} alt={elem.name} />
-      </div>
-      <div className='divCart'>
-        <div className='titleCart'>
-          <p>{elem.name}</p>
-          <BiTrash className='trash' onClick={() => {removeCard(elem)}}/>
-        </div>
-        <div className='countCart'>
-          <p>-</p>
-          <div className='white'><p>0</p></div>
-          <p>+</p>
-        </div>
-      </div>
-    </LiCard>
-  )
+    return (
+        <LiCard>
+            <div className='divImg'>
+                <img src={elem.img} alt={elem.name} />
+            </div>
+            <div className='divCart'>
+                <div className='titleCart'>
+                    <p>{elem.name}</p>
+                    <BiTrash
+                        className='trash'
+                        onClick={() => {
+                            removeCard(elem)
+                        }}
+                    />
+                </div>
+            </div>
+        </LiCard>
+    )
 }
 
 export default CardCart
